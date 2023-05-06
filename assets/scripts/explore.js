adba9f1a8235c38b3ca56db6c4ae17a821136770
Prash Katukojwala <31807747+prashasthk@users.noreply.github.com>
2023-05-05 17:19:55 -0700

    explore pt 1 fixed

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,27 +1,33 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+let voices = [];
 
 
 function init() {
     const synth = window.speechSynthesis;
-    const voices = synth.getVoices();
-    console.log(voices);
-    for (let i = 0; i < voices.length; i++) {
-        const option = document.createElement("option");
-        option.textContent = `${voices[i].name} (${voices[i].lang})`;
-        if (voices[i].default) {
-          option.textContent += " — DEFAULT";
-        }
-    
-        option.setAttribute("data-lang", voices[i].lang);
-        option.setAttribute("data-name", voices[i].name);
-        document.getElementById("voice-select").appendChild(option);
-    }
+    synth.addEventListener("voiceschanged", function() {
+      voices = synth.getVoices();
+      for (let i = 0; i < voices.length; i++) {
+          const option = document.createElement("option");
+          option.textContent = `${voices[i].name} (${voices[i].lang})`;
+          if (voices[i].default) {
+            option.textContent += " — DEFAULT";
+          }
+          option.setAttribute("data-lang", voices[i].lang);
+          option.setAttribute("data-name", voices[i].name);
+          document.getElementById("voice-select").appendChild(option);
+      }
+    });
 
     document.querySelector("button").addEventListener('click', function() {
         const speech = new SpeechSynthesisUtterance(document.getElementById("text-to-speak").value);
+        const selectedOption = document.getElementById("voice-select").selectedOptions[0].getAttribute("data-name");
+        for (let i = 0; i < voices.length; i++) {
+          if (voices[i].name === selectedOption) {
+              speech.voice = voices[i];
+          }
+        }
         speechSynthesis.speak(speech);
         document.querySelector("img").src = "assets/images/smiling-open.png"; 
     });
